277f9f5ba3e2e977eed3aa5d2df0e890fa148ec2
numaru <kin.numaru@laposte.net>
2017-03-13 21:43:42 +0100

    Check for errors during win32 calls

--- injector.py
+++ injector.py
@@ -1,5 +1,6 @@
 import subprocess
-from ctypes import byref, c_int, c_long, c_ulong, create_string_buffer, windll
+from ctypes import (WinError, byref, c_int, c_long, c_ulong,
+                    create_string_buffer, windll)
 
 
 class Injector:
@@ -21,24 +22,35 @@ class Injector:
         self.unload()
         self.pid = c_ulong(pid)
         self.handle = self.kernel32.OpenProcess(self.PROC_ALL_ACCESS, 0, pid)
+        if not self.handle:
+            raise WinError()
 
     def unload(self):
         if self.handle:
             self.kernel32.CloseHandle(self.handle)
+            if not self.handle:
+                raise WinError()
         self.handle = None
 
     def alloc_remote(self, buffer, size):
         alloc = self.kernel32.VirtualAllocEx(self.handle, None, c_int(size),
                                              self.MEM_CREATE, self.PAGE_EX)
+        if not alloc:
+            raise WinError()
         self.write_memory(alloc, buffer)
         return alloc
 
     def free_remote(self, addr, size):
-        self.kernel32.VirtualFreeEx(self.handle, addr, c_int(size), self.MEM_RELEASE)
+        if not self.kernel32.VirtualFreeEx(self.handle, addr, c_int(size), self.MEM_RELEASE):
+            raise WinError()
 
     def get_address_from_module(self, module, function):
         module_addr = self.kernel32.GetModuleHandleA(module.encode("ascii"))
+        if not module_addr:
+            raise WinError()
         function_addr = self.kernel32.GetProcAddress(module_addr, function.encode("ascii"))
+        if not module_addr:
+            raise WinError()
         return function_addr
 
     def create_remote_thread(self, function_addr, args):
@@ -46,19 +58,25 @@ class Injector:
         args_addr = self.alloc_remote(args, len(args))
         thread = self.kernel32.CreateRemoteThread(self.handle, None, None, c_long(function_addr),
                                                   c_long(args_addr), None, None)
-        self.kernel32.WaitForSingleObject(thread, 0xFFFFFFFF)
-        self.kernel32.GetExitCodeThread(thread, byref(dll_addr))
+        if not thread:
+            raise WinError()
+        if self.kernel32.WaitForSingleObject(thread, 0xFFFFFFFF) == 0xFFFFFFFF:
+            raise WinError()
+        if not self.kernel32.GetExitCodeThread(thread, byref(dll_addr)):
+            return WinError()
         self.free_remote(args_addr, len(args))
         return dll_addr.value
 
     def read_memory(self, addr, size):
         buffer = create_string_buffer(size)
-        self.kernel32.ReadProcessMemory(self.handle, c_long(addr), buffer, size, None)
+        if not self.kernel32.ReadProcessMemory(self.handle, c_long(addr), buffer, size, None):
+            raise WinError()
         return buffer
 
     def write_memory(self, addr, string):
         size = len(string)
-        self.kernel32.WriteProcessMemory(self.handle, addr, string, size, None)
+        if not self.kernel32.WriteProcessMemory(self.handle, addr, string, size, None):
+            raise WinError()
 
     def load_library(self, buffer):
         function_addr = self.get_address_from_module("kernel32.dll", "LoadLibraryA")
@@ -70,13 +88,15 @@ class Injector:
 
     def call_from_injected(self, path, dll_addr, function, args):
         function_offset = self.get_offset_of_exported_function(path.encode("ascii"), function)
-        if function_offset:
-            self.create_remote_thread(dll_addr + function_offset, args)
+        self.create_remote_thread(dll_addr + function_offset, args)
 
     def get_offset_of_exported_function(self, module, function):
         base_addr = self.kernel32.LoadLibraryA(module)
         if not base_addr:
-            return 0
+            return WinError()
         function_addr = self.kernel32.GetProcAddress(base_addr, function.encode("ascii"))
-        self.kernel32.FreeLibrary(base_addr)
+        if not function_addr:
+            raise WinError()
+        if not self.kernel32.FreeLibrary(base_addr):
+            raise WinError()
         return function_addr - base_addr
